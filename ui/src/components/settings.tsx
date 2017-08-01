8cc8bf9a0fe725555c4bd005d4553ce769deed5f
Craig Peterson <cpeterson@stackoveflow.com>
2017-08-01 16:16:09 -0400

    slightly more

--- ui/src/components/settings.tsx
+++ ui/src/components/settings.tsx
@@ -7,13 +7,53 @@ interface SettingsProps {
     Settings: Setting[];
 }
 
-export class Settings extends React.Component<SettingsProps, {}>{
 
+export class Settings extends React.Component<SettingsProps, {}>{
     public render() {
+        var mainStyle ={
+            border: "1px solid #ddd",
+            borderTop: "none",
+        }
+        var groupStyle = {
+            boxSizing: 'border-box',
+        };
+        var headStyle = {
+            padding: "5px 8px",
+            fontWeight: 700 as 700,
+            backgroundColor: "#eee",
+            borderTop: "1px solid #ddd",
+            margin: 0,
+        }
         var groups = _(this.props.Settings).groupBy<string>((s) => s.name.split('.')[0])
             .map((k: Setting[], v: string) => {
-                return <h1 key={v}>{v}</h1>
+                return <div style={groupStyle} key={v}><h4 style={headStyle}>{v}</h4>
+                    {k.map((s) => <SingleSetting key={s.name} Setting={s} />)}
+                </div>
             }).value();
-        return <div>{groups}</div>
+        return <div style={mainStyle}>{groups}</div>
+    }
+}
+
+interface SingleSettingProps {
+    Setting: Setting;
+}
+
+class SingleSetting extends React.Component<SingleSettingProps, {}>{
+
+    public render() {
+        var s = this.props.Setting;
+        var rowStyle = {
+            paddingTop: 0,
+            paddingBottom: 0,
+            display: "flex",
+            flexDirection: "row" as "row",
+            flexWrap: "wrap" as "wrap",
+            alignItems: "stretch"as "stretch",
+            borderTop: "1px solid #ddd",
+            cursor: "default",
+        }
+        return <div style={rowStyle}>
+            <div><a><strong>{s.name}</strong></a></div>
+        </div>
     }
 }
